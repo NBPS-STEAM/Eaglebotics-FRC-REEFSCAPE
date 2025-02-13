85910f82ffc8eff83df83c57fb7c30ff7cb7d159
Gelter317 <justin_pleffner@mynbps.org>
2025-02-12 20:37:45 -0500

    Fixed mistake

--- src/main/java/frc/robot/commands/oldordrivecommands/ScoreCommands/TelePathingCommands.java
+++ src/main/java/frc/robot/commands/oldordrivecommands/ScoreCommands/TelePathingCommands.java
@@ -1,3 +1,8 @@
+package frc.robot.commands.oldordrivecommands.ScoreCommands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+
 public class TelePathingCommands {
 
     
