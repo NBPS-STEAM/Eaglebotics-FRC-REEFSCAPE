32fe1f1d642f758669ab3d245dca98d770e6a7ad
Gelter317 <justin_pleffner@mynbps.org>
2025-02-21 19:11:20 -0500

    Commented and fixed path math

--- src/main/java/frc/robot/commands/oldordrivecommands/ScoreCommands/TelePathingCommands.java
+++ src/main/java/frc/robot/commands/oldordrivecommands/ScoreCommands/TelePathingCommands.java
@@ -70,20 +70,26 @@ public class TelePathingCommands {
         PathPlannerPath path;
      
 
+        //The current position of the robot
         double roboX = swerve.getPose().getX();
         double roboY = swerve.getPose().getY();
 
+        //The final position the robot will end at ((4.5, 4.0) is the center of the reef)
         double targX = 1.75 * Math.cos(index * Math.PI / 3) + 4.5;
         double targY = 1.75 * Math.sin(index * Math.PI / 3) + 4.0;
 
-        double slope = -Math.tan(index * Math.PI / 3);
-        System.out.println(slope);
+        //This calculates a line, parallel to the reef,
+        //and centered at the targetpoint. Then it calculates the distance
+        //of the x position of the robot from the x position of the line
+        //at the y position of the robot (The x(y))
+        double tangent = -Math.tan(index * Math.PI / 3);
+        double pointDifference = roboX - (targX + tangent*(roboY-targY));
 
-        double pointDifference = roboX - (targX + slope*(roboY-targY));
-        System.out.println(pointDifference);
+        double slope = index * Math.PI / 3;
 
-        slope = index * Math.PI / 3;
-
+        //If the index is 0, 1, or 5, the robot needs to be to the right
+        //of the line created above to be able to get to the final point
+        //in a straight line. 
         boolean canGoStraight;
         if (index <= 1 || index == 5) {
             canGoStraight = pointDifference > 0;
@@ -92,6 +98,7 @@ public class TelePathingCommands {
         }
 
 
+        //The code before the 'else' is basically the same as the simple path
         if (canGoStraight) {
 
 
@@ -116,29 +123,37 @@ public class TelePathingCommands {
 
         } else {
 
+            //Creates the angle from the center of the reef to the robot
             double angle = Math.atan2(roboY - 4.0, roboX - 4.5);
             if (angle < 0) {
                 angle += Math.PI*2;
             }
-            boolean goClockwise;
+            boolean goCounterCW;
 
+            //Determines which direction to go. Honestly I'm not 100% sure why it works
             if (angle > slope) {
-                double clockwise = Math.abs(angle - (slope + 2 * Math.PI));
-                double counterCW = Math.abs(angle - slope);
+                double counterCW = Math.abs(angle - (slope + 2 * Math.PI));
+                double clockwise = Math.abs(angle - slope);
 
-                goClockwise = clockwise < counterCW;
+                goCounterCW = counterCW < clockwise;
             } else {
                 double clockwise = Math.abs(angle - slope);
                 double counterCW = Math.abs(angle - (slope - 2 * Math.PI));
                 System.out.println(angle - (slope - 2 * Math.PI));
 
-                goClockwise = clockwise < counterCW;
+                goCounterCW = clockwise < counterCW;
             }
 
             ArrayList<Double> angleList = new ArrayList<>();
 
 
-            if (goClockwise) {
+            //This is the really rough part, but it works.
+            //Based on the angles and whether it can go clockwise, it chooses which code to run
+            //Each one is fairly similar with slightly different parameters.
+            //It loops through each possible index point, and determines whether its 'in the way'
+            //by seeing if its between the angle of the robot and the angle of the target point.
+            //If so, it adds it to angleList
+            if (goCounterCW) {
                 if (angle > slope) {
                     System.out.println(1);
                     for (int i = 0; i < 6; i++) {
@@ -172,10 +187,14 @@ public class TelePathingCommands {
                 }
             }
 
+            //This converts the indexes gotten above into their distances from the robot
+            //then it sorts them by value (smallest/closest first).
+            //It does not actually change the values, as the final list is still a list of angles
             ArrayList<Double> sortedAngleList = angleList.stream()
                     .sorted(Comparator.comparing(ang -> Math.hypot(2.15 * Math.cos(ang) + 4.5 - roboX, 2.15 * Math.sin(ang) + 4.0 - roboY)))
                     .collect(Collectors.toCollection(ArrayList::new));
             
+            //Creates a list of poses from each angle from sortedAngleList
             ArrayList<Pose2d> poseList = new ArrayList<>();
             for (Double ang : sortedAngleList) {
                 poseList.add(new Pose2d(
@@ -184,6 +203,12 @@ public class TelePathingCommands {
                     Rotation2d.fromRadians(ang + Math.PI/2)
                 ));
             }
+            poseList.add(new Pose2d(
+                1.75*Math.cos(index * Math.PI/3) + 4.5,
+                1.75*Math.sin(index * Math.PI/3) + 4.0,
+                Rotation2d.fromDegrees(180 + index*60))
+            );
+
             List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(poseList);
 
             PathConstraints constraints = new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI); // The constraints for this path.
