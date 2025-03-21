0a9db131dc93da2bd0f55df5d994eeeeb25f17db
Gelter317 <justin_pleffner@mynbps.org>
2025-03-21 19:20:37 -0400

    LED methods for modes (coral/algae)

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -28,10 +28,35 @@ public class LEDSubsystem extends SubsystemBase{
         m_Blinken.set(0.67);//gold
     }
     public void setOuttake(){
-        m_Blinken.set(0.07);//flashes whatever we set color 1 to fast
+        m_Blinken.set(0.07);//flashes whatever we set color 1 as to fast
     }
     public void setIntake(){
-        m_Blinken.set(0.27);//flashes whatever we set color 2 to fast
+        m_Blinken.set(0.27);//flashes whatever we set color 2 as to fast
+    }
+
+
+
+    public void setAlgaeMode(){
+        m_Blinken.set(0.79); //Green-Blue
+    }
+    public void setCoralMode(){
+        m_Blinken.set(0.63); //Red-Blue
+    }
+
+    //Called when the robot begins traveling to a set position, blinks whichever color the robot was already in
+    public void setGoingToPos(){
+        if (m_Blinken.get() == 0.79 || m_Blinken.get() == 0.07) {
+
+            m_Blinken.set(0.07); //Fast heartbeat for color 1 which should be Blue-Green
+
+        } else if (m_Blinken.get() == 0.63 || m_Blinken.get() == 0.27) {
+
+            m_Blinken.set(0.27); //Fast heatbeat for color 2 which should be Red-Orange
+
+        } else {
+
+            m_Blinken.set(-0.87); //If the other two for some reason fail, Confetti!!
+        }
     }
     
 }
